1a3897fd35f1de2dc76cb04c9098d34a7ada8cf8
Tung.dang <>
2020-03-03 11:36:43 +0700

    123

--- TestJenkin/ViewController.swift
+++ TestJenkin/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+// dang thanh tug
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
